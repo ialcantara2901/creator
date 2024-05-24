f08cd3d47a8e8ecc121e37f5d75324e0bf0c16ae
Igor Alcantara <igor.alcantara@aalc.com.br>
2024-05-23 23:55:39 -0300

    Retorn commit add git

--- handler.py
+++ handler.py
@@ -1,19 +1,22 @@
 # handler.py
 
 from service import RabbitMQService
-import os, json
+import os
+import subprocess
 
 class FileCreationHandler:
     def __init__(self):
         self.rabbitmq_service = RabbitMQService()
 
     def create_file(self, filename, content):
-        # Certifique-se de que o diretório existe, se aplicável
-        directory = os.path.dirname(filename)
+        # Certifique-se de que o diretório ./tmp existe
+        filepath = os.path.join('tmp', filename)
+        directory = os.path.dirname(filepath)
         if directory and not os.path.exists(directory):
             os.makedirs(directory, exist_ok=True)
-        with open(filename, 'w', encoding='utf-8') as file:
+        with open(filepath, 'w', encoding='utf-8') as file:
             file.write(content)
+
     def process_input(self, input_data):
         # Carregar o JSON
         data = input_data
@@ -23,64 +26,66 @@ class FileCreationHandler:
         self.create_file('handler.py', data['handler'])
         self.create_file('application.py', data['application'])
         self.create_file('.env', data['env'])
+        # Criar requirements.txt na pasta ./tmp
+        self.create_file('requirements.txt', data['requirements'])
 
         # Criar Dockerfile
         dockerfile_content = '''
-        # Dockerfile
-        FROM python:3.9-alpine
-        
-        WORKDIR /app
-        
-        COPY . /app
-        
-        # Instale as dependências Python
-        RUN pip install --upgrade pip
-        RUN pip install pika
-        RUN pip install python-dotenv
-        
-        CMD ["python", "application.py"]
+# Dockerfile
+FROM python:3.9-alpine
+
+WORKDIR /app
+
+COPY . /app
+
+# Instale as dependências Python
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+
+CMD ["python", "application.py"]
         '''
         self.create_file('Dockerfile', dockerfile_content)
 
         # Criar arquivo de deploy (docker-compose.yml para Swarm)
         docker_compose_content = '''
-        version: '3.8'
-        services:
-          url-status-checker:
-            image: url_status_checker:latest
-            build: .
-            deploy:
-              replicas: 1
-              restart_policy:
-                condition: on-failure
-            networks:
-              - url-net
-        networks:
-          url-net:
+version: '3.8'
+services:
+  url-status-checker:
+    image: igoralcantara/app:v0.01
+    build: .
+    deploy:
+      replicas: 1
+      restart_policy:
+        condition: on-failure
+    networks:
+      - minha_rede
+networks:
+  minha_rede:
+    external: true
         '''
         self.create_file('docker-compose.yml', docker_compose_content)
 
         # Criar arquivo publish-image.yaml
         publish_image_content = '''
-        name: Publish image docker hub
-        on:
-        push:
-            branches:
-            - 'main'
+name: Publish image docker hub
+on:
+push:
+    branches:
+    - 'main'
 
-        jobs:
-        publish_image:
-            runs-on: ubuntu-latest
-            steps:
-            - name: checkout
-                uses: actions/checkout@v4
-            - name: build
-                run: |
-                docker build . -t igoralcantara/app:creator1-v0.01
-            - name: publish
-                run: |
-                docker login -u igoralcantara -p ${{ secrets.DOCKER_HUB_TOKEN }}
-                docker push igoralcantara/app:creator1-v0.01
+jobs:
+publish_image:
+    runs-on: ubuntu-latest
+    steps:
+    - name: checkout
+        uses: actions/checkout@v4
+    - name: build
+        run: |
+        docker build . -t igoralcantara/app:v0.01
+    - name: publish
+        run: |
+        docker login -u igoralcantara -p ${{ secrets.DOCKER_HUB_TOKEN }}
+        docker push igoralcantara/app:v0.01
         '''
         self.create_file('.github/workflows/publish-image.yaml', publish_image_content)
 
@@ -88,6 +93,31 @@ class FileCreationHandler:
         
         # Retornar uma mensagem de sucesso
         return {"status": "success", "message": "Files created successfully"}
+
+    def run_git_commands(self):
+        #github_username = os.getenv('GITHUB_USERNAME')
+        github_username = "ialcantara2901"
+        #github_token = os.getenv('GITHUB_TOKEN')
+        g_token = "${{ secrets.G_TOKEN }}"
+        repository_url = f'https://{github_username}:{g_token}@github.com/{github_username}/creator.git'
+        
+        commands = [
+            "git init",
+            f"git remote add origin {repository_url}",
+            "git add .",
+            'git commit -m "Start IA app"',
+            "git branch -M main",
+            "git push -u origin main"
+        ]
+        cwd = './tmp'
+        for command in commands:
+            subprocess.run(command, cwd=cwd, shell=True, check=True)
+        print("Comandos Git executados com sucesso.")
+        
+        cwd = './tmp'
+        for command in commands:
+            subprocess.run(command, cwd=cwd, shell=True, check=True)
+        print("Comandos Git executados com sucesso.")
 
     def handle_message(self, message):
         result = self.process_input(message)
